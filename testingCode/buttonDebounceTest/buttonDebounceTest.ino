07a641c6d5fd1fa4f833dd117bc0d30253de32d5
ilan3r <108901514+ilan3r@users.noreply.github.com>
2024-06-25 22:09:03 -0400

    test the menu display, button debounce updates
    created and tested the display of the menu in menuTest.ino
    
    making changes to buttonDebounceTest.ino, unsuccessful in fixing the button debouncing and getting accurate hold and idle times

---
+++ testingCode/buttonDebounceTest/buttonDebounceTest.ino
@@ -0,0 +1,96 @@
+/*
+Date: May 25th 2024 
+Description: test buttons and button debouncing
+make sure one button press isn't misinterpreted as multiple bounces 
+reusing some code from morse code project 
+
+*/
+
+     
+
+const int okButton = 7; 
+const int backButton = 8;
+
+
+long unsigned currentTime = 0; 
+
+
+// the time allowed for button debouncing 
+const int debounceDelay = 10; 
+
+long lastDetection; 
+
+// the current and previous state of the button
+int okButtonState = 0;     
+int okLastButtonState = 0; 
+bool okPressed = 0; 
+
+// the moment the button was pressed and released
+int startPressed = 0;    
+int endPressed = 0;      
+
+// the time the button has held for and been released for 
+unsigned long holdTime = 0;  
+unsigned long idleTime = 0; 
+
+void setup() {
+  Serial.begin(9600); 
+  pinMode(okButton, INPUT_PULLUP); 
+  pinMode(backButton, INPUT_PULLUP); 
+
+}
+
+void loop() {
+  int okReading = digitalRead(okButton); 
+
+
+  // to check how long it has been since user has done anything 
+  if (okReading != okLastButtonState){
+    lastDetection = millis();
+  }
+
+  // only accept the change in button state after debounce time
+  if ( (millis() - lastDetection) > debounceDelay ){
+    okButtonState = okReading; 
+  }
+
+  // if the button is pressed or released
+  if (okButtonState != okLastButtonState)
+  {
+
+    // if the button is just pressed 
+    if (okButtonState == 0)
+    {
+      startPressed = millis(); 
+
+      idleTime = millis() - endPressed; 
+
+      if (okButtonState == 0 & okPressed == 1)
+      {
+        Serial.print("======just released======== hold time: ");
+        Serial.println(holdTime); 
+      }
+      okPressed = 0; 
+
+    }
+
+    // if the button is just released 
+    else if (okButtonState == 1)
+    {
+      endPressed = millis(); 
+      holdTime = millis() - startPressed;
+      if (okButtonState == 1 && okPressed == 0)
+      {
+        Serial.print("---just pressed--- idle time: ");
+        Serial.println(idleTime);
+        // Serial.println("-------ok button press");
+      }
+      okPressed = 1; 
+    }
+
+
+  }
+  okLastButtonState = okReading; 
+
+
+}
--- testingCode/menuTest/menuTest.ino
+++ testingCode/menuTest/menuTest.ino
@@ -1,13 +1,15 @@
 /*
 Date: June 25th 2024 
-Description: test stopwatch display on OLED, format properly with min:seconds.milliseconds 
+Description: test the menu and interaction with the buttons 
 
 */
+
 
 #include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
@@ -21,69 +23,37 @@ Description: test stopwatch display on OLED, format properly with min:seconds.mi
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 void setup() {
-
   // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
   if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
     Serial.println(F("SSD1306 allocation failed"));
     for(;;); // Don't proceed, loop forever
   }
+  display.clearDisplay();
   display.display();
-
 
 }
 
 void loop() {
 
-  display.clearDisplay();
 
 
-  // at this size, assuming that we won't reach 10 minutes 
-  display.setTextSize(3);             
+  // first row 
+  display.setTextSize(2);             
   display.setTextColor(SSD1306_WHITE);        
-  display.setCursor(0,0);             // Start at top-left corner
+  display.setCursor(0,0);            
 
+  display.print("mode ");
+  display.setCursor(90,0);
+  // display.print("stp");
+  display.print("tmr");
 
-  float timeSec = millis()/1000.0; 
-  int min = 0; 
-
-  while (timeSec >= 60.0)
-  {
-    timeSec = timeSec - 60.0; 
-    min++; 
-  }
   
-  if (min > 0)
-  {
-    display.print(min);
-    display.print(":");
-
-    if (timeSec < 10.0)
-    {
-      display.print("0");
-      display.print(timeSec); 
-      display.display();
-    }
-    else
-    {
-      display.print(timeSec);
-      display.display();
-    }
-  }
-  else{
-    if (timeSec < 10.0)
-    {
-      display.print("0");
-      display.print(timeSec); 
-      display.display();
-    }
-
-    else
-    {
-      display.println(millis()/1000.0, 2);
-      display.display();
-
-    }
-  }
-  delay(10);
+  // second row 
+  display.setCursor(0,32); 
+  display.print("output");
+  display.setCursor(90,32);
+  // display.print("bzz");
+  display.print("rng");
+  display.display();
 
 }
